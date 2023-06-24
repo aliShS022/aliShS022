a5f00e7d8c26f489655784da21835e066b92943f
unknown <shabaisuorosh@gmail.com>
2023-06-24 14:17:18 -0700

    initial backend commit

---
+++ Novin.FoodApp.Infrastructure/Data/NovinFoodAppDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Novin.Foodapp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novin.FoodApp.Infrastructure.Data
+{
+    public class NovinFoodAppDB : DbContext
+    {
+        public DbSet<ApplicationUsers> ApplicationUsers { get; set; }
+        public NovinFoodAppDB(DbContextOptions<NovinFoodAppDB> option)
+            : base(option)
+        {
+            
+        }
+    }
+}
---
+++ Novin.Foodapp.Core/Entities/BaseEntiti.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novin.Foodapp.Core.Entities
+{
+    public class BaseEntiti
+    {
+        public virtual int Id { get; set; }
+    }
+}
